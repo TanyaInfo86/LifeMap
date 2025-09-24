cb55a0d87170955303a1c238e3ea709abc3aa48f
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-09-24 22:39:56 +0300

    fix textlist

--- LifeMap/src/redux/store.js
+++ LifeMap/src/redux/store.js
@@ -1,3 +1,28 @@
+// import { configureStore } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   tasks: {
+//     items: [
+//       { id: 0, text: 'Learn HTML and CSS', completed: true },
+//       { id: 1, text: 'Get good at JavaScript', completed: true },
+//       { id: 2, text: 'Master React', completed: false },
+//       { id: 3, text: 'Discover Redux', completed: false },
+//       { id: 4, text: 'Build amazing apps', completed: false },
+//     ],
+//   },
+//   filters: {
+//     status: 'all',
+//   },
+// };
+
+// const rootReducer = (state = initialState, action) => {
+//   return state;
+// };
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
+
 import { configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
@@ -16,7 +41,51 @@ const initialState = {
 };
 
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'tasks/addTask': {
+      return {
+        ...state,
+        tasks: {
+          items: [...state.tasks.items, action.payload],
+        },
+      };
+    }
+
+    case 'tasks/deleteTask':
+      return {
+        ...state,
+        tasks: {
+          items: state.tasks.items.filter((task) => task.id !== action.payload),
+        },
+      };
+
+    case 'tasks/toggleCompleted':
+      return {
+        ...state,
+        tasks: {
+          items: state.tasks.items.map((task) => {
+            if (task.id !== action.payload) {
+              return task;
+            }
+            return {
+              ...task,
+              completed: !task.completed,
+            };
+          }),
+        },
+      };
+
+    case 'filters/setStatusFilter':
+      return {
+        ...state,
+        filters: {
+          status: action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
 };
 
 export const store = configureStore({
